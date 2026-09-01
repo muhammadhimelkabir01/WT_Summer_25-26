a361856dcb5affdd87a4ead1ee36fe6be5ce58e4
ananyaswapneel10-source <ananyaswapneel10@gmail.com>
2026-09-01 12:45:47 +0600

    homecontroller.php

---
+++ campus-share/app/controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once __DIR__ . '/../models/Resource.php';
+
+class HomeController {
+    private $resourceModel;
+
+    public function __construct() {
+        $this->resourceModel = new Resource();
+    }
+
+       public function index() {
+        $keyword = trim($_GET['search'] ?? '');
+        $category = $_GET['category'] ?? '';
+        $type = $_GET['type'] ?? '';
+
+        $categories = $this->resourceModel->getAllCategories();
+        $resources = $this->resourceModel->searchAndFilter($keyword, $category, $type);
+
+        require_once __DIR__ . '/../views/guest/home.php';
+    }
