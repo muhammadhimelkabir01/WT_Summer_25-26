5d5c82379ced572cfeeb04a7aba75f2601feef1b
ananyaswapneel10-source <ananyaswapneel10@gmail.com>
2026-09-01 23:59:23 +0600

    home.php

---
+++ campus-share/app/views/guest/home.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . '/../layouts/header.php';
+?>
+
+<div style="text-align: center; margin-bottom: 25px;">
+    <h1 style="font-size: 26px; color: #003366; margin-bottom: 6px;">Intra-Campus Peer-to-Peer Resource Sharing</h1>
+    <p style="color: #64748b; font-size: 14px;">Rent academic equipment, calculators, lab kits, or claim free donated books from peers.</p>
+</div>
