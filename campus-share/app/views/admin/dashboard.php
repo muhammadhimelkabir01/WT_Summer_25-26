6b8c217cda5759a0f5af4f76a8adf3e6ce5b2267
Shanjida Sultana <shanjidasultana1459@gmail.com>
2026-09-01 22:10:34 +0600

    Add admin_dashboard

---
+++ campus-share/app/views/admin/dashboard.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once __DIR__ . '/../layouts/header.php';
+?>
+
+<style>
+    .admin-header {
+        margin-bottom: 25px;
+    }
+    .admin-header h2 {
+        color: #003366;
+        font-size: 24px;
+        font-weight: 700;
+        margin-bottom: 4px;
+    }
