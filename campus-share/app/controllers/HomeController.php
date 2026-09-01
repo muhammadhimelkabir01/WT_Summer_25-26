de299e9ab9cc5a1305c15b70b02515fbae3be54a
ananyaswapneel10-source <ananyaswapneel10@gmail.com>
2026-09-01 23:17:38 +0600

    add homecontroller.php

--- campus-share/app/controllers/HomeController.php
+++ campus-share/app/controllers/HomeController.php
@@ -8,3 +8,14 @@ class HomeController {
     public function __construct() {
         $this->resourceModel = new Resource();
     }
+
+    public function index() {
+        $keyword = trim($_GET['search'] ?? '');
+        $category = $_GET['category'] ?? '';
+        $type = $_GET['type'] ?? '';
+
+        $categories = $this->resourceModel->getAllCategories();
+        $resources = $this->resourceModel->searchAndFilter($keyword, $category, $type);
+
+        require_once __DIR__ . '/../views/guest/home.php';
+    }
